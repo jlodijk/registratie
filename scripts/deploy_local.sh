e646038c2e4e16617ef101dd2fd6395dc1071787
jlodijk <jlodijk@gmail.com>
2026-02-05 11:59:15 +0100

    Use docker env file and update deps for deploy

--- scripts/deploy_local.sh
+++ scripts/deploy_local.sh
@@ -3,11 +3,18 @@ set -euo pipefail
 
 REMOTE_USER="deploy"
 REMOTE_HOST="116.203.242.75"
-REMOTE_PATH="/home/deploy/"
+REMOTE_PATH="/home/deploy/registratie"
 BRANCH="master"
+LOCAL_REMOTE="origin"
+REMOTE_REMOTE="origin"
 
 # Push lokale wijzigingen
-git push origin "$BRANCH"
+git push "${LOCAL_REMOTE}" "$BRANCH"
 
-# Pull + deploy op de server
-ssh "${REMOTE_USER}@${REMOTE_HOST}" "cd ${REMOTE_PATH} && git pull origin ${BRANCH} && ./deploy.sh"
+# Pull + deploy op de server (deploy.sh staat één niveau hoger)
+ssh "${REMOTE_USER}@${REMOTE_HOST}" "\
+  cd ${REMOTE_PATH} && \
+  git pull ${REMOTE_REMOTE} ${BRANCH} && \
+  git clean -fd && \
+  cd .. && \
+  ./deploy.sh"
